faecea87a09a848d752dfb8dc360e1f156d8dfad
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-02 23:31:14 +0000

    fix issue with promise chain

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL,WEATHER_API_KEY } from './api';
+
+import { useState } from 'react';
+
 
 function App() {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [currentForecast, setCurrentForecast] = useState(null);
   const handleOnSearchChange = (searchData) =>  {
-    console.log(searchData)
-    console.log("HI");
+
+    const [lat, lon] = searchData.value.split(",");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+    const currentForecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([currentWeatherFetch, currentForecastFetch])
+      .then(async (response) => {
+        const currentWeather = await response[0].json();
+        const currentForecast = await response[1].json();
+        setCurrentWeather({city: searchData.label,  ...currentWeather});
+        setCurrentForecast({city: searchData.label,  ...currentForecast});
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      
   }
+
+  console.log(currentWeather, currentForecast);
 
   return (
     <div className="container">
--- src/api.js
+++ src/api.js
@@ -7,3 +7,8 @@ export const geoAPIOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "679d4b6a44eabcdebdfab8dbdaf70401"
+
+
