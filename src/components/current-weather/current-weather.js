7882aca8dd72efa0e6d146008ce3860edb6c36d8
dannygorgon <thirdoptionisthename@googlemail.com>
2023-12-02 17:24:42 +0000

    add bottom section with temperature, wind, humidity and pressure information

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -4,11 +4,35 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <div >
+        <div>
           <p className="city">London</p>
-          <p className="weather-desc">Raining</p>
+          <p className="weather-description">Raining</p>
         </div>
-      <img className="" src="icons/09d.png" alt="" />
+        <img className="weather-icon" src="icons/09d.png" alt="" />
+      </div>
+      <div className="bottom">
+        <p className="temperature">0°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value">-1°C</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">10%</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
+        </div>
       </div>
     </div>
   );
